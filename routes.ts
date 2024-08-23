6c1b961be6aeadd0bdbfe75b8156334516920288
vinex-cyber <vinex71@gmail.com>
2024-08-23 17:35:25 +0700

    prisma and middleware

---
+++ routes.ts
@@ -0,0 +1,27 @@
+/**
+ * An array of routes that are accessible to the public
+ * These routes do not require authentication
+ * @type {string[]}
+ */
+
+export const publicRoutes = ["/"];
+
+/**
+ * An array of routes that are accessible used for authentication
+ * These routes will redirect logged in users to / settings
+ * @type {string[]}
+ */
+export const authRoutes = ["auth/login", "auth/register"];
+
+/**
+ * The prefix for API authentication routes
+ * Routes that start whit this prefix are used for API authentication
+ * @type {string}
+ */
+export const apiAuthPrefix = "/api/auth";
+
+/**
+ * The default login redirect
+ * @type {string}
+ */
+export const DEFAULT_LOGIN_REDIRECT = "/settings";
